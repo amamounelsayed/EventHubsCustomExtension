972e1e3cf6d9d6d55304dd3149ea7c64ce451be5
Ahmed El Sayed (Mamoun) <52262708+amamounelsayed@users.noreply.github.com>
2019-09-16 12:22:38 -0700

    -Init work

---
+++ EventHubsCustomExtension/EventHubsExtensionConfigProvider.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.WebJobs.EventHubs;
+using Microsoft.Azure.WebJobs.Host.Config;
+using Newtonsoft.Json.Linq;
+
+namespace EventHubsCustomExtension
+{
+    internal class EventHubCustomExtensionConfigProvider : IExtensionConfigProvider
+    {
+        public void Initialize(ExtensionConfigContext context)
+        {
+            context.AddConverter<byte[], EventData>(ConvertBytesToEventData);
+        }
+
+        private static EventData ConvertBytesToEventData(byte[] bytes)
+        {
+            string input = Encoding.UTF8.GetString(bytes);
+
+            if (IsJsonObject(input))
+            {
+                try
+                {
+                    // attempt to parse as an EventDataEx
+                    JObject o = JObject.Parse(input);
+                    var partitionKey = (string)o.GetValue("PartitionKey", StringComparison.OrdinalIgnoreCase)?.Value<string>();
+
+                    
+                    if (!string.IsNullOrEmpty(partitionKey))
+                    {
+                        byte[] body = o.GetValue("Body", StringComparison.OrdinalIgnoreCase)?.ToObject<byte[]>();
+                        return new EventDataEx(body)
+                        {
+                            PartitionKey = partitionKey
+                        };
+                    }
+                }
+                catch
+                {
+                    // best effort
+                }
+            }
+
+            // return default EventData
+            return new EventData(bytes);
+        }
+
+        private static bool IsJsonObject(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            input = input.Trim();
+            return (input.StartsWith("{", StringComparison.OrdinalIgnoreCase) && input.EndsWith("}", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
---
+++ EventHubsCustomExtension/Startup.cs
@@ -0,0 +1,20 @@
+﻿using EventHubsCustomExtension;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.Hosting;
+
+[assembly: WebJobsStartup(typeof(EventHubsCustomExtensionStartup))]
+
+namespace EventHubsCustomExtension
+{
+    public class EventHubsCustomExtensionStartup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            // if this extension is registered AFTER the base extension
+            // it's converters will replace the built in converters
+            // (ConverterManager logic is replace/last one wins)
+            builder.AddExtension<EventHubCustomExtensionConfigProvider>();
+        }
+    }
+}
